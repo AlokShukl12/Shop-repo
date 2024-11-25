811893cac8653e8262ea0855320fff9ad46353c8
Alok Shukla <alokshukla6060@gmail.com>
2024-11-25 12:48:00 +0530

    first commit

---
+++ frontend/src/pages/Cashback.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cashback = () => {
+  return (
+    <><div className='bg-red-500 font-bold'>Cashback</div><div className='bg-red-600 font-semibold'>This part under processing!</div></>
+  )
+}
+
+export default Cashback
